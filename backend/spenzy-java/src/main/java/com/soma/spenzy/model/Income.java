e6f1423912f86f330c35ff80a46610562b841528
somavrg <varga.j.soma@gmail.com>
2024-01-08 14:06:26 +0100

    feat: create Income model class

---
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/Income.java
@@ -0,0 +1,33 @@
+package com.soma.spenzy.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Income {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int amount;
+
+    private LocalDateTime date;
+
+    @ManyToOne
+    private SpenzyUser user;
+
+    // should be optional
+    private String info;
+}
