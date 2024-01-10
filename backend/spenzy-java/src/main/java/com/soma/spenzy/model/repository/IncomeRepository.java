906a596785c165eeb04d61d0424afb13809ac047
somavrg <varga.j.soma@gmail.com>
2024-01-10 13:08:55 +0100

    feat: fix IncomeRepository with find instead of get

--- backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/IncomeRepository.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/IncomeRepository.java
@@ -7,6 +7,6 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
-    Set<Income> getIncomesByUserId(Long userId);
-    Set<Income> getIncomesByUserIdAndDateBetween(Long userId, LocalDateTime start, LocalDateTime end);
+    Set<Income> findIncomesByUserId(Long userId);
+    Set<Income> findIncomesByUserIdAndDateBetween(Long userId, LocalDateTime start, LocalDateTime end);
 }
