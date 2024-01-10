9569da9b4c768b40da5ff768b5179c25cc250889
somavrg <varga.j.soma@gmail.com>
2024-01-10 13:09:12 +0100

    feat: fix SubscriptionRepository with find instead of get

--- backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/SubscriptionRepository.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/SubscriptionRepository.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-    Set<Subscription> getSubscriptionsByUserId(Long userId);
-    Set<Subscription> getSubscriptionsByUserIdAndDateToPayBefore(Long userId, LocalDateTime date);
-    Set<Subscription> getSubscriptionsByUserIdAndDateToPayBetween(Long userId, LocalDateTime start, LocalDateTime end);
+    Set<Subscription> findSubscriptionsByUserId(Long userId);
+    Set<Subscription> findSubscriptionsByUserIdAndDateToPayBefore(Long userId, LocalDateTime date);
+    Set<Subscription> findSubscriptionsByUserIdAndDateToPayBetween(Long userId, LocalDateTime start, LocalDateTime end);
 }
