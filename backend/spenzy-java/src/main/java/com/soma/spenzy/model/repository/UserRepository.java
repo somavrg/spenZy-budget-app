b303c5e83141891c16203dcf78cdb881184b672b
somavrg <varga.j.soma@gmail.com>
2024-01-16 09:35:57 +0100

    feat: create UserRepository interface

--- backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/UserRepository.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.soma.spenzy.model.repository;
 import com.soma.spenzy.model.SpenzyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SpenzyUser, Long> {
+    Optional<SpenzyUser> findByEmail(String email);
+    SpenzyUser deleteByEmail(String email);
 }
