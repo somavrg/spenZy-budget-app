1a2b1edc9e075a164b39f8155b7ff9b78f8579ed
somavrg <varga.j.soma@gmail.com>
2024-02-06 11:38:11 +0100

    feat: create SubscriptionService interface

---
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/service/subscriptionservice/SubscriptionService.java
@@ -0,0 +1,15 @@
+package com.soma.spenzy.service.subscriptionservice;
+
+import com.soma.spenzy.model.DTO.NewSubscriptionDTO;
+import com.soma.spenzy.model.DTO.SubscriptionDTO;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public interface SubscriptionService {
+    Set<SubscriptionDTO> getAllSubscriptions(String token);
+    Set<SubscriptionDTO> getSubscriptionsBetweenDates(String token, LocalDateTime start, LocalDateTime end);
+    Set<SubscriptionDTO> getSubscriptionsBeforeDate(String token, LocalDateTime date);
+    NewSubscriptionDTO addNewSubscription(NewSubscriptionDTO newSubscriptionDTO);
+    void deleteSubscription(Long subscriptionId);
+}
