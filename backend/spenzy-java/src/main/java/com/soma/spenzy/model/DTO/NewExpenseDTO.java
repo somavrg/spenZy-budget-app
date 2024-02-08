3d17c534fd7d66e006c9f7574ad3c5e99407c950
somavrg <varga.j.soma@gmail.com>
2024-02-08 10:18:10 +0100

    fix: fix ExpenseType field

--- backend/spenzy-java/src/main/java/com/soma/spenzy/model/DTO/NewExpenseDTO.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/DTO/NewExpenseDTO.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public record NewExpenseDTO(
         String name,
-        ExpenseType type,
+        ExpenseType expenseType,
         int amount,
         LocalDateTime date,
         //optional?
