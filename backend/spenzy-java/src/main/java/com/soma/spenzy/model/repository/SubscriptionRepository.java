2a1995449d490d461fa7c63b62e511d85e8014a8
somavrg <varga.j.soma@gmail.com>
2024-02-08 10:09:53 +0100

    feat: fix repository with findAll

--- backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/SubscriptionRepository.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/SubscriptionRepository.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-    Set<Subscription> findSubscriptionsByUser_Email(String email);
-    Set<Subscription> findSubscriptionsByUser_EmailAndDateToPayBefore(String email, LocalDateTime date);
-    Set<Subscription> findSubscriptionsByUser_EmailAndDateToPayBetween(String email, LocalDateTime start, LocalDateTime end);
+    Set<Subscription> findAllByUser_Email(String email);
+    Set<Subscription> findAllByUser_EmailAndDateToPayBefore(String email, LocalDateTime date);
+    Set<Subscription> findAllByUser_EmailAndDateToPayBetween(String email, LocalDateTime start, LocalDateTime end);
 }
