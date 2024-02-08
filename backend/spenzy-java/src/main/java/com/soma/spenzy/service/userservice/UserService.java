40e304cef029bd38e2a4ea7d014584e3da7a51de
somavrg <varga.j.soma@gmail.com>
2024-02-08 10:58:38 +0100

    feat: fix return types for methods

--- backend/spenzy-java/src/main/java/com/soma/spenzy/service/userservice/UserService.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/service/userservice/UserService.java
@@ -1,16 +1,14 @@
 package com.soma.spenzy.service.userservice;
 
 import com.soma.spenzy.model.DTO.UserDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 
 import java.util.Set;
 
 public interface UserService {
-    ResponseEntity<Set<UserDTO>> getAllUsers();
-    ResponseEntity<UserDTO> getUserById(String token);
-    ResponseEntity<UserDTO> getUserByEmail(String email);
-    ResponseEntity<UserDTO> addNewUser(UserDTO userDTO);
-    ResponseEntity<UserDTO> updateUser(String token, UserDTO userDTO);
-    HttpStatus deleteUser(String token);
+    Set<UserDTO> getAllUsers();
+    UserDTO getUserByEmail(String token);
+    UserDTO addNewUser(UserDTO userDTO);
+    UserDTO updateUser(String token, UserDTO userDTO);
+    void deleteUser(String token);
 }
