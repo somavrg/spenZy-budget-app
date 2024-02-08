50c86c57b89c920cf99100f3f6dba89e295a69a7
somavrg <varga.j.soma@gmail.com>
2024-02-08 10:07:34 +0100

    feat: fix repository with findAll

--- backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/IncomeRepository.java
+++ backend/spenzy-java/src/main/java/com/soma/spenzy/model/repository/IncomeRepository.java
@@ -7,6 +7,6 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
-    Set<Income> findIncomesByUser_Email(String email);
-    Set<Income> findIncomesByUser_EmailAndDateBetween(String email, LocalDateTime start, LocalDateTime end);
+    Set<Income> findAllByUser_Email(String email);
+    Set<Income> findAllByUser_EmailAndDateBetween(String email, LocalDateTime start, LocalDateTime end);
 }
